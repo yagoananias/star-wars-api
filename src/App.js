308f3ea0dc847392189692fe21be597effde9edc
Yago Ananias <yago_souza92@yahoo.com.br>
2020-06-11 14:22:48 -0300

    Finalizando Página People

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+import Navbar from './componentes/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Container, Dimmer, Loader } from 'semantic-ui-react'
+import Home from './componentes/Home'
+import People from './componentes/People'
+import Planets from './componentes/Planets'
+
 
 function App() {
+  const [people, setPeople] = useState([])
+  const [planets, setPlanets] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json')
+      let data = await res.json()
+      setPeople(data.results)
+    }
+
+    async function fetchPlanets() {
+      let res = await fetch('https://swapi.dev/api/planets/?format=json')
+      let data = await res.json()
+      setPlanets(data.results)
+    }
+
+    fetchPeople()
+    fetchPlanets()
+    setLoading(false)
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Container>
+          {loading ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          ) : (
+            <Switch>
+            <Route exact path='/'>
+              <Home/>
+            </Route>
+            <Route exact path='/people'>
+              <People data={people} />
+            </Route>
+            <Route exact path='/planets'>
+              <Planets data={planets} />
+            </Route>
+          </Switch>
+          )}
+          
+        </Container>
+      </Router>
+    </>
   );
 }
 
